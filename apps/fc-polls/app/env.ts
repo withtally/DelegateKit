0a2cd3a337682669d2dfb4a475d8de0198059854
dawsbot <dawsonbotsford@gmail.com>
2024-05-15 12:04:00 -0600

    more logging and HOST fixes

--- apps/fc-polls/app/env.ts
+++ apps/fc-polls/app/env.ts
@@ -1,14 +1,31 @@
 /* eslint-disable no-process-env */
 import { z } from "zod";
 const VERCEL_PROJECT_PRODUCTION_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL;
+const VERCEL_BRANCH_URL = process.env.VERCEL_BRANCH_URL;
+const VERCEL_ENV = process.env.VERCEL_ENV;
+
+const VERCEL_HOST =
+  VERCEL_ENV === "production"
+    ? VERCEL_PROJECT_PRODUCTION_URL
+    : VERCEL_ENV === "preview"
+      ? VERCEL_BRANCH_URL
+      : null;
+
 export const env = {
-  HOST: VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${VERCEL_PROJECT_PRODUCTION_URL}`
-    : z.string().parse(process.env.HOST),
-  HUB_URL: z.string().parse(process.env.HUB_URL),
+  HOST: VERCEL_HOST
+    ? `https://${VERCEL_HOST}`
+    : z.string().url().parse(process.env.HOST),
+  HUB_URL: z
+    .string()
+    .refine((hubUrl) => !hubUrl.startsWith("http"), {
+      message: '"HUB_URL" must not begin with "http"',
+    })
+    .parse(process.env.HUB_URL),
 } as const;
 
 console.log({
+  URL: VERCEL_HOST,
+  VERCEL_ENV,
   HOST: env.HOST,
   VERCEL_URL: process.env.VERCEL_URL,
   VERCEL_PROJECT_PRODUCTION_URL: process.env.VERCEL_PROJECT_PRODUCTION_URL,
