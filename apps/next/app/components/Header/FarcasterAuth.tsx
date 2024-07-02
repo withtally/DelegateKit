88bc5720f5ebc46022b908ee52dc88febe27d67b
dawsbot <3408480+dawsbot@users.noreply.github.com>
2024-07-02 11:52:56 -0600

    Address selection

--- apps/next/app/components/Header/FarcasterAuth.tsx
+++ apps/next/app/components/Header/FarcasterAuth.tsx
@@ -1,16 +1,57 @@
 import { SignInButton } from "@farcaster/auth-kit";
 import "@farcaster/auth-kit/styles.css";
+import Select, { SingleValue } from "react-select";
 import { useSetAddress } from "../../hooks/use-set-address";
 
 const FarcasterAuth = () => {
-  const { address, verifiedAddresses } = useSetAddress();
+  const { address, setAddress, verifiedAddresses } = useSetAddress();
+  const selectOptions = verifiedAddresses.map((verifiedAddress) => ({
+    value: verifiedAddress,
+    label: verifiedAddress,
+  }));
+
+  const onChange = (
+    newValue: SingleValue<{ value: `0x${string}`; label: `0x${string}` }>,
+  ) => {
+    if (newValue === null) {
+      return;
+    }
+    setAddress(newValue.value);
+  };
+
   return (
     <>
       <SignInButton />
-      selected address: {address}
-      <br />
-      <br />
-      verified addreses: {verifiedAddresses.join("\n")}
+      {address && (
+        <>
+          Selected address:
+          <Select
+            className="basic-single"
+            defaultValue={selectOptions.find(
+              (option) => option.value === address,
+            )}
+            classNamePrefix="select"
+            isDisabled={false}
+            isClearable={false}
+            isRtl={false}
+            isSearchable={false}
+            name="color"
+            options={selectOptions}
+            onChange={onChange}
+          />
+          <p>
+            Don&apos;t see your delegate address here?{" "}
+            <a
+              href="https://warpcast.com/~/settings/verified-addresses"
+              target="_blank"
+              className="underline"
+            >
+              Verify your address on Warpcast
+            </a>
+            , return here, and sign in to refresh addresses.
+          </p>
+        </>
+      )}
     </>
   );
 };
