c4826dab61bf33365ea309870989eaea08d6bffe
dawsbot <3408480+dawsbot@users.noreply.github.com>
2024-07-19 14:07:20 -0600

    resove ens image

--- apps/next/pages/api/optimism/delegates/images/1.tsx
+++ apps/next/pages/api/optimism/delegates/images/1.tsx
@@ -1,7 +1,7 @@
 import { NextApiRequest } from "next";
 import { ImageResponse } from "next/og";
 import * as v from "valibot";
-import { fetchAddressImage } from "../../../../../app/delegates/[address]/fetch-address-image";
+import { ENS } from "../../../../../src/api/ENS/ENS";
 import { OPDelegate } from "../../../../../src/api/services/OPDelegate/OPDelegate";
 import { frameHeight, frameWidth } from "../../../frame-config";
 import { DelegateImageOnly } from "./1-image";
@@ -11,7 +11,7 @@ export const runtime = "edge";
 export default async function Frame1(req: NextApiRequest) {
   const url = new URL(v.parse(v.string([v.url()]), req.url));
   const address = v.parse(v.string(), url.searchParams.get("address"));
-  const avatar = fetchAddressImage(address);
+  const avatar = (await ENS.getENSAvatar(address)) || "";
   const opDelegate = new OPDelegate(address);
   const delegateData = await opDelegate.fetchAgoraData();
   return new ImageResponse(
