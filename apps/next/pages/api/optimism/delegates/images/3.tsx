f72b7ade4a5a4f653df01bf6de77764bb0825c5c
dawsbot <3408480+dawsbot@users.noreply.github.com>
2024-08-04 20:53:25 -0400

    Unstyled badges on slide 3 of delegate

--- apps/next/pages/api/optimism/delegates/images/3.tsx
+++ apps/next/pages/api/optimism/delegates/images/3.tsx
@@ -3,6 +3,7 @@ import { ImageResponse } from "next/og";
 import { z } from "zod";
 import { fetchAddressImage } from "../../../../../app/delegates/[address]/fetch-address-image";
 import { fetchDelegateData } from "../../../../../app/delegates/[address]/fetchers";
+import { routes } from "../../../../../app/routes";
 import DelegateImageContainer from "../../../../../src/api/delegates/DelegateImageContainer";
 import { frameHeight, frameWidth } from "../../../frame-config";
 
@@ -19,6 +20,101 @@ export default async function Frame2(req: NextApiRequest) {
     (
       <DelegateImageContainer>
         <img
+          src={routes.images.op}
+          alt="op logo"
+          width="50px"
+          height="50px"
+          style={{
+            position: "relative",
+            left: "56px",
+            top: "66px",
+          }}
+        />
+        <img
+          src={delegateImage}
+          width="80px"
+          height="80px"
+          style={{
+            borderRadius: "50%",
+            height: "80px",
+            width: "80px",
+            position: "relative",
+            left: "100px",
+          }}
+          alt="profile"
+        />
+        <img
+          alt="star"
+          src={routes.images.star}
+          width="40px"
+          height="40px"
+          style={{ position: "absolute", right: "80px", bottom: "100px" }}
+        />
+        <img
+          alt="star"
+          src={routes.images.star}
+          width="20px"
+          height="20px"
+          style={{
+            position: "absolute",
+            right: "280px",
+            top: "130px",
+            transform: `rotate(40deg)`,
+          }}
+        />
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            // {
+            // position: "relative", top: "60px",
+            // left: "00px",
+            // display: "flex",
+            // alignItems: "center",
+            // justifyContent: "center",
+            // textAlign: "center",
+            // }
+          }}
+        >
+          <p style={{ color: "grey", fontSize: "20px" }}>Delegate Statistics</p>
+          <br />
+          <h3
+            style={{
+              border: "1px solid grey",
+              borderRadius: "6px",
+              padding: "6px 18px",
+              alignSelf: "flex-start",
+              fontSize: "20px",
+            }}
+          >
+            {delegateData.proposalsVotedOn} Votes
+          </h3>
+          <h3
+            style={{
+              border: "1px solid grey",
+              borderRadius: "6px",
+              padding: "6px 18px",
+              alignSelf: "flex-start",
+              fontSize: "20px",
+            }}
+          >
+            Created {delegateData.proposalsCreated} Proposals
+          </h3>
+          <h3
+            style={{
+              border: "1px solid grey",
+              borderRadius: "6px",
+              padding: "6px 18px",
+              alignSelf: "flex-start",
+              fontSize: "20px",
+            }}
+          >
+            Delegated from{" "}
+            {Number(delegateData.numOfDelegators).toLocaleString("en-US")} OP
+            holders
+          </h3>
+        </div>
+        {/* <img
           src={delegateImage}
           style={{
             borderRadius: "50%",
@@ -45,11 +141,11 @@ export default async function Frame2(req: NextApiRequest) {
             Recent Activity:
             {delegateData.votingParticipation}
           </span> */}
-          <span>Proposals Created: {delegateData.proposalsCreated}</span>
+        {/* <span>Proposals Created: {delegateData.proposalsCreated}</span>
           <span>
             Delegated From: {delegateData.numOfDelegators.toLocaleString()}
           </span>
-        </div>
+        </div> */}
       </DelegateImageContainer>
     ),
     {
