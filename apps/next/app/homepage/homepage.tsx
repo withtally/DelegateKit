cd61ddeb14ce23f5ca8599e10762498679d936f9
dawsbot <3408480+dawsbot@users.noreply.github.com>
2024-08-06 15:51:49 -0400

    poll homepage section

--- apps/next/app/homepage/homepage.tsx
+++ apps/next/app/homepage/homepage.tsx
@@ -1,5 +1,7 @@
 import dynamic from "next/dynamic";
 import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../components/Button";
 import MobileTopImageSrc from "./mobile-top.svg";
 const LoginButtonWithNoSSR = dynamic(
   () => import("../components/LoginButton"),
@@ -11,7 +13,7 @@ const LeftColumn = ({ children }: { children: React.ReactNode }) => {
   return <div className="">{children}</div>;
 };
 const RightColumn = ({ children }: { children: React.ReactNode }) => {
-  return <div className="lg:w-1/2">{children}</div>;
+  return <div className="lg:w-1/2 py-12">{children}</div>;
 };
 
 export const Home = () => {
@@ -38,6 +40,7 @@ export const Home = () => {
             <p>
               The missing connection between Optimism Governance and Farcaster
             </p>
+            <div className="pt-8"></div>
             <LoginButtonWithNoSSR />
           </div>
         </LeftColumn>
@@ -46,12 +49,12 @@ export const Home = () => {
             src={MobileTopImageSrc}
             alt="backdrop"
             style={{ width: "100%", borderRadius: "24px" }}
-            className="mb-[-100px] md:mb-0 lg:h-[800px]"
+            className="mb-[-100px] md:mb-0 lg:h-[600px]"
           />
         </RightColumn>
       </section>
       {/* three column section */}
-      <section className="flex px-0 md:px-24 flex-col lg:flex-row lg:space-x-10 space-y-10 lg:space-y-0">
+      <section className="pb-0 lg:pb-12 flex px-0 md:px-24 flex-col lg:flex-row lg:space-x-10 space-y-10 lg:space-y-0">
         <div className="lg:w-1/3 pt-16 lg:pt-0">
           <Image
             src="/images/homepage/polls.svg"
@@ -91,6 +94,37 @@ export const Home = () => {
             <p>Link directly to a contentious proposal</p>
           </h2>
         </div>
+      </section>
+      <section className="py-24 px-0 md:px-24 flex text-left flex-col-reverse lg:flex-row justify-between">
+        <LeftColumn>
+          <Image
+            src="/images/homepage/polls-demo.svg"
+            width={500}
+            height={500}
+            alt="polls demo"
+          />
+        </LeftColumn>
+        <RightColumn>
+          <Image
+            src="/images/homepage/polls.svg"
+            width={120}
+            height={120}
+            alt="chat-bubble"
+          />
+          <h2 style={{ fontSize: "48px" }}>
+            {" "}
+            Get feedback
+            <br />
+            quickly and easily.
+          </h2>
+          <p className="py-6">
+            Represent your followers best on-chain by first polling for their
+            opinions!
+          </p>
+          <Link href="/polls/new">
+            <Button secondary>Create a Poll</Button>
+          </Link>
+        </RightColumn>
       </section>
     </div>
   );
